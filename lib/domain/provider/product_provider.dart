9d5ca48df5a7185a4754c94cb9ad6d1ade2deab9
KainFlorian <flo.kainrath@gmail.com>
2022-03-03 11:21:16 +0100

    changed image

--- lib/domain/provider/product_provider.dart
+++ lib/domain/provider/product_provider.dart
@@ -10,11 +10,11 @@ class ProductProvider with ChangeNotifier {
     Product("bigmac", 23, "test",
         "https://www.mcdonalds.at/wp-content/uploads/2021/09/web-neu-1500x1500-core-big-mac-1.png"),
     Product("whopper", 23, "test",
-        "https://banner2.cleanpng.com/20180131/afq/kisspng-whopper-hamburger-cheeseburger-burger-king-premium-fast-food-burger-5a725b35edda78.0012443215174438939743.jpg"),
+        "https://www.handelsblatt.com/images/der-rebel-whopper/25214366/2-formatOriginal.png"),
   ];
 
   List<Product> filterByFavourite(bool isFavourite) {
-    if(isFavourite) {
+    if (isFavourite) {
       return products.where((product) => product.favourite).toList();
     } else {
       return products;
